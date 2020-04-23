242af1ef76c69563f6b933dc55f26c40c60bddbb
Jetreck <Johannes.hoefl@gmx.at>
2020-04-23 20:38:30 +0200

    mein push ;)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 const runChallenge1 = () => {
   var isPrincessHappy = false;
   // Write your code below this
-
+    isPrincessHappy = true;
 
   // Write your code above this
   if (isPrincessHappy) {
@@ -17,7 +17,7 @@ const runChallenge1 = () => {
 const runChallenge2 = () => {
   var princessName = '';
   // Write your code below this
-
+    princessName = 'Veronica';
 
   // Write your code above this
   if (princessName === 'Veronica') {
@@ -28,7 +28,7 @@ const runChallenge2 = () => {
 const runChallenge3 = (princessBirthYear) => {
   var princessAge;
   // Write your code below this
-
+    princessAge = 34;
 
 
   // Write your code above this
