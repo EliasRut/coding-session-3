244bd2eee8de9daab98db088d42789d7e802792e
Elias Rut <eli4srut@gmail.com>
2020-04-26 09:01:06 +0200

    Restructured page to include actual buttons, and added three new challenges.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const runChallenge1 = () => {
@@ -9,7 +8,8 @@ const runChallenge1 = () => {
 
   // Write your code above this
   if (isPrincessHappy) {
-    console.log('You have solved the first challenge!');
+    var outputElement = document.getElementById('challenge1Output');
+    outputElement.innerHTML = 'You have solved the first challenge!';
   }
 }
 
@@ -20,7 +20,8 @@ const runChallenge2 = () => {
 
   // Write your code above this
   if (princessName === 'Veronica') {
-    console.log('You have solved the second challenge!');
+    var outputElement = document.getElementById('challenge2Output');
+    outputElement.innerHTML = 'You have solved the second challenge!';
   }
 }
 
@@ -28,11 +29,13 @@ const runChallenge3 = (princessBirthYear) => {
   var princessAge;
   // Write your code below this
 
+  // Write the princesses Age, depending on princessBirthYear
 
 
   // Write your code above this
   if (princessAge === 34) {
-    console.log('You have solved the third challenge!');
+    var outputElement = document.getElementById('challenge3Output');
+    outputElement.innerHTML = 'You have solved the third challenge!';
   }
 }
 
@@ -44,7 +47,8 @@ const runChallenge4 = () => {
 
   // Write your code above this
   if (princesses.length >= 4) {
-    console.log('You have solved the fourth challenge!');
+    var outputElement = document.getElementById('challenge4Output');
+    outputElement.innerHTML = 'You have solved the fourth challenge!';
   }
 }
 
@@ -56,7 +60,8 @@ const runChallenge5 = () => {
 
   // Write your code above this
   if (princesses[0] === 'Veronica') {
-    console.log('You have solved the fifth challenge!');
+    var outputElement = document.getElementById('challenge5Output');
+    outputElement.innerHTML = 'You have solved the fifth challenge!';
   }
 }
 
@@ -64,37 +69,168 @@ const runChallenge6 = () => {
   var princesses = ['Anna', 'Syndi', 'Theodora'];
   // Write your code below this
 
+  princesses = 0
 
 
   // Write your code above this
   if (princesses.length === 0) {
-    console.log('You have solved the sixth challenge!');
+    var outputElement = document.getElementById('challenge6Output');
+    outputElement.innerHTML = 'You have solved the sixth challenge!';
   }
 }
 
+const runChallenge7 = () => {
+  var princesses = ['Anna', 'Syndi', 'Theodora'];
+  var inputElement = document.getElementById('challenge7Input');
+  // eslint-disable-next-line
+  var inputValue = inputElement.value; 
+  // Write your code below this
+
+  // Add the name in the input to the array of princesses.
+
+
+  // Write your code above this
+  if (princesses.length === 0) {
+    var outputElement = document.getElementById('challenge7Output');
+    outputElement.innerHTML = 'You have solved the seventh challenge!';
+  }
+}
+
+const runChallenge8 = () => {
+  var inputElement = document.getElementById('challenge8Input');
+  // eslint-disable-next-line
+  var inputValue = inputElement.value; 
+  // Write your code below this
+  // Sorry, no automatic grading - you need to know if you solved this one yourself
+
+  // Output the text "Dragon is happy" if the input value is 'Veronica',
+  // otherwise output 'Not the right princess'.
+
+  // Write your code above this
+}
+
+const runChallenge9 = () => {
+  // eslint-disable-next-line
+  const listOfOfficialDisneyPrincesses = [
+    'SnowWhite',
+    'Cinderella',
+    'Aurora',
+    'Ariel',
+    'Belle',
+    'Jasmine',
+    'Pocahontas',
+    'Mulan',
+    'Tiana',
+    'Rapunzel',
+    'Merida',
+    'Moana'
+  ];
+  var inputElement = document.getElementById('challenge9Input');
+  var inputValue = inputElement.value;
+  var inputNumber = Number(inputValue);
+  if (isNaN(inputNumber)) {
+    var outputElement = document.getElementById('challenge9Output');
+    outputElement.innerHTML = 'Error: Not a number.';
+  }
+  // Write your code below this
+  // Sorry, no automatic grading - you need to know if you solved this one yourself
+
+  // Output the name of the disney princess matching the input number - make sure to check for
+  // cases where there is no number accociated with a princess, and display some error if that
+  // happens. Think about what the edge cases are and make sur to test those.
+
+  // Write your code above this
+}
+
 function App() {
-  runChallenge1();
-  runChallenge2();
-  runChallenge3(1986);
-  runChallenge4();
-  runChallenge5();
-  runChallenge6();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <div className="Challenge">
+          <div className="header">Challenge 1</div>
+          <button onClick={() => runChallenge1()}>Run</button>
+          <div>
+            <div className="outputHeader">Output:</div>
+            <div className="outputSpace" id="challenge1Output"></div>
+          </div>
+        </div>
+        <div className="Challenge">
+          <div className="header">Challenge 2</div>
+          <button onClick={() => runChallenge2()}>Run</button>
+          <div>
+            <div className="outputHeader">Output:</div>
+            <div className="outputSpace" id="challenge2Output"></div>
+          </div>
+        </div>
+        <div className="Challenge">
+          <div className="header">Challenge 3</div>
+          <button onClick={() => runChallenge3(1982)}>Run</button>
+          <div>
+            <div className="outputHeader">Output:</div>
+            <div className="outputSpace" id="challenge3Output"></div>
+          </div>
+        </div>
+        <div className="Challenge">
+          <div className="header">Challenge 4</div>
+          <button onClick={() => runChallenge4()}>Run</button>
+          <div>
+            <div className="outputHeader">Output:</div>
+            <div className="outputSpace" id="challenge4Output"></div>
+          </div>
+        </div>
+        <div className="Challenge">
+          <div className="header">Challenge 5</div>
+          <button onClick={() => runChallenge5()}>Run</button>
+          <div>
+            <div className="outputHeader">Output:</div>
+            <div className="outputSpace" id="challenge5Output"></div>
+          </div>
+        </div>
+        <div className="Challenge">
+          <div className="header">Challenge 6</div>
+          <button onClick={() => runChallenge6()}>Run</button>
+          <div>
+            <div className="outputHeader">Output:</div>
+            <div className="outputSpace" id="challenge6Output"></div>
+          </div>
+        </div>
+        <div className="Challenge">
+          <div className="header">Challenge 7</div>
+          <div>
+            <div>Princess name:</div>
+            <input type="text" id="challenge7Input" />
+          </div>
+          <button onClick={() => runChallenge7()}>Run</button>
+          <div>
+            <div className="outputHeader">Output:</div>
+            <div className="outputSpace" id="challenge7Output"></div>
+          </div>
+        </div>
+        <div className="Challenge">
+          <div className="header">Challenge 8</div>
+          <div>
+            <div>Princess name:</div>
+            <input type="text" id="challenge8Input" />
+          </div>
+          <button onClick={() => runChallenge8()}>Run</button>
+          <div>
+            <div className="outputHeader">Output:</div>
+            <div className="outputSpace" id="challenge8Output"></div>
+          </div>
+        </div>
+        <div className="Challenge">
+          <div className="header">Challenge 9</div>
+          <div>
+            <div>Princess number:</div>
+            <input type="text" id="challenge9Input" />
+          </div>
+          <button onClick={() => runChallenge9()}>Run</button>
+          <div>
+            <div className="outputHeader">Output:</div>
+            <div className="outputSpace" id="challenge9Output"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
